609ea9602f0fa7fd112642aec889f12efa3d9c53
Cyril-777 <kyrillosmaher10@gmail.com>
2022-12-08 09:14:40 -0800

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Size of char: %i bytes\n", sizeof(char));
+	printf("Size of int: %i bytes\n", sizeof(int));
+	printf("Size of long int: %i bytes\n", sizeof(long int));
+	printf("Size of float: %i bytes\n", sizeof(float));
+	printf("Size of double: %i bytes\n", sizeof(double));
+	return (0);
+}
