cc1cb7c7fbbbecc59210d18ea9dd5b2e0b2b3e26
Cyril-777 <kyrillosmaher10@gmail.com>
2022-12-08 09:19:25 -0800

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry
+ * return = 0
+ */
 int main(void)
 {
 	printf("Size of char: %i bytes\n", sizeof(char));
 	printf("Size of int: %i bytes\n", sizeof(int));
 	printf("Size of long int: %i bytes\n", sizeof(long int));
+	printf("Size of long long int: %i bytes\n", sizeof(long long int));
 	printf("Size of float: %i bytes\n", sizeof(float));
-	printf("Size of double: %i bytes\n", sizeof(double));
 	return (0);
 }
