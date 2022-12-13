180a065d6b372295a60b83d2be68c5867def6c9b
Cyril-777 <kyrillosmaher10@gmail.com>
2022-12-13 15:39:43 -0800

    mm

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * jack_bauer- prints every min
+ */
+void jack_bauer(void)
+{
+	int min, hr;
+
+	for (hr = 0; hr <= 23; h++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar(min);
+			_putchar('\n');
+		}
+	}
+}
