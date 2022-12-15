ab46b8a42a98326f3cdbf8f12ceb5424e6fe4995
Cyril-777 <kyrillosmaher10@gmail.com>
2022-12-15 08:10:17 -0800

    simple operations

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for digit
+ * @c: int
+ * Return: 1 if not return 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - funct to multiply two int
+ * @a: first num
+ * @B: second num
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
