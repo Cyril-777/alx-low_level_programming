66620daa00165ab13be41ecde42816fc684685e2
Cyril-777 <kyrillosmaher10@gmail.com>
2022-12-19 06:40:21 -0800

    mm

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - fuc to reverse a string.
+ *@s: arg
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+	int i = 0;
+	char *cp = s;
+	int x;
+    int a = 0;
+	char c;
+
+	while (*cp != '\0')
+	{
+		cp++;
+		length++;
+	}
+
+	i = length - 1;
+	for (a < ((i / 2) + 1); a++)
+	{
+		x = (i - a);
+		c = s[a];
+		s[a] = s[x];
+		s[x] = c;
+	}
+}
