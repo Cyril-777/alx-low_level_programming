13e3f698e9050e23a400c69ed366546382a749c0
Cyril-777 <kyrillosmaher10@gmail.com>
2022-12-21 13:40:39 -0800

    first task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - conc 2 strings
+ * @src: source string
+ * @dest: destination string
+ *
+ * Return: pointer to the resulting string dest.
+ **/
+ 
+char *_strcat(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
+}
