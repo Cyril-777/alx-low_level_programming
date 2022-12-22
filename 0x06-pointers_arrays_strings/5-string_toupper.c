361f0f2e42623f57d1fe6f593d5c975769e43a76
Cyril-777 <kyrillosmaher10@gmail.com>
2022-12-22 09:07:53 -0800

    4-5 tasks

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * string_toupper - changes lowercase letters to uppercase
+  * @str: String to return
+  * Return: String
+  */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		str[i] = toupper(str[i]);
+		i++;
+	}
+	return (str);
+}
