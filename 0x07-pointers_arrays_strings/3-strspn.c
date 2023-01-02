1f30811b82a377f41faf50e79feb8a5628ea0420
Cyril-777 <kyrillosmaher10@gmail.com>
2023-01-02 04:29:48 -0800

    80 line limit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,9 @@
  * _strspn - Get the length of a prefix substring
  * @s: string
  * @accept: string
- * Return: Returns the number of bytes in the initial segment of s which consist only of bytes from accept
+ * Return: Returns the number of bytes
+ * in the initial segment of s
+ * which consist only of bytes from accept
  */
 
 unsigned int _strspn(char *s, char *accept)
