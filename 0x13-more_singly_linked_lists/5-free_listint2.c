f29948384c2255434ff460af641773451b320357
Cyril-777 <kyrillosmaher10@gmail.com>
2023-01-30 11:17:17 -0800

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list
+ *  andsets the head to NULL
+ * @head: ptr to ptr listint_t
+ **/
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	if (head == NULL)
+		return (NULL);
+	if ((*head)->next != NULL)
+		ptr = (*head)->next;
+	else
+	{
+		free(*head);
+		*head = NULL;
+	}
+	if (*head)
+	{
+		free_listint2(&ptr);
+		free(*head);
+	}
+	*head = NULL;
+}
