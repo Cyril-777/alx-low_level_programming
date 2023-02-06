92f225f372adbb8852be361bbde8b117fb6ff4fe
Cyril-777 <kyrillosmaher10@gmail.com>
2023-02-06 08:54:47 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,49 @@
 #include "main.h"
 #include <stddef.h>
 /**
- * create_file: func to create a file
+ * _strlen - counts string length
+ * @str: string to be used
+ *
+ * Return: length of the string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+/**
+ * create_file - func to create a file
  * description: short describe
  * @filename: self ex
  * @text_content: self ex
- * Return: 0
+ * Return: 1 on succsess or -1 fail
  **/
 int create_file(const char *filename, char *text_content)
 {
+	int file, wrote;
+
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-	FILE *file = fopen(filename, "w");
-
-	if (file == NULL)
-	{
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
-	}
-
-	chmod(filename, S_IRUSR | S_IWUSR);
-
 	if (text_content != NULL)
 	{
-		fprintf(file, "%s", text_content);
+		wrote = write(file, text_content, _strlen(text_content));
+		if (wrote == -1)
+		{
+			close(file);
+			return (-1);
+		}
+		close(file);
+		return (1);
 	}
-	fclose(file);
-	return (1);
+	else
+	{
+		close(file);
+		return (1);
+	}
 }
