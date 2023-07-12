4232c14008ec76375ed4e2ae69ebd886ec158831
Cyril-777 <kyrillosmaher10@gmail.com>
2023-07-12 07:32:38 -0700

    betty style fixed

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,30 +11,33 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-    int left = 0;
-    int right = size - 1;
+	int left = 0;
+	int right = size - 1;
 
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
 
-        printf("Searching in array: ");
-        for (int i = left; i <= right; i++) {
-            printf("%d", array[i]);
-            if (i != right)
-                printf(", ");
-        }
-        printf("\n");
+		printf("Searching in array: ");
+		for (int i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i != right)
+				printf(", ");
+		}
+		printf("\n");
 
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-
-    return (-1);
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
 }
